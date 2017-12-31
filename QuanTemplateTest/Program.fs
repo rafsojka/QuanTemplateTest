cd64d7f3a6a35d827cd3d5b5d203dabb2312c1cd
rafsojka <raf.sojka@gmail.com>
2017-12-31 21:12:43 +0000

    introducing OrderBook abstract class

--- QuanTemplateTest/Program.fs
+++ QuanTemplateTest/Program.fs
@@ -4,128 +4,22 @@ module QuanTemplateTest.Main
 
 open System
 
-type Instruction = |   Update   |   Delete  |   New
-
-type Side = |   Bid |   Ask
-
-type OrderBookUpdate =
-    {   Instruction     :   Instruction
-        Side            :   Side
-        PriceLevelIndex :   int
-        Price           :   int
-        Quantity        :   int
-    }
-    static member FromOrderBookUpdateFileLine (line:string) =
-        let tokens = line.Split(' ')
-        let instruction =
-            match tokens.[0] with
-            | "U" -> Update
-            | "D" -> Delete
-            | "N" -> New
-            | _ -> invalidArg line "Invalid Instruction string"
-        let side = 
-            match tokens.[1] with
-            | "B" -> Bid
-            | "A" -> Ask
-            | _ -> invalidArg line "Invalid Side string"
-        {   Instruction = instruction
-            Side = side
-            PriceLevelIndex = Convert.ToInt32(tokens.[2])
-            Price = Convert.ToInt32(tokens.[3])
-            Quantity = Convert.ToInt32(tokens.[4])  }
-
-type OrderBookEntry =
-    {   Price       :   decimal
-        Quantity    :   int }   
-    static member Default =
-        {   Price = 0.0M
-            Quantity = 0    }
-    static member Update price quantity =
-        {   Price = price
-            Quantity = quantity    }
-
- // O(1)
-let updateOrderBook orderBookUpdate (orderBook:OrderBookEntry []) tickSize =
-    orderBook.[orderBookUpdate.PriceLevelIndex - 1] <- OrderBookEntry.Update ((decimal)orderBookUpdate.Price * tickSize) orderBookUpdate.Quantity
-
-let updateOrderBook2 orderBookUpdate tickSize (orderBook:OrderBookEntry [])  =
-    Array.set orderBook (orderBookUpdate.PriceLevelIndex - 1) (OrderBookEntry.Update ((decimal)orderBookUpdate.Price * tickSize) orderBookUpdate.Quantity)
-    orderBook
-
-// O(n)
-// to improve look at 
-// https://hamberg.no/erlend/posts/2012-08-29-purely-functional-random-access-list.html
-// https://gist.github.com/kos59125/3721051
-let insertIntoOrderBook orderBookUpdate (orderBook:OrderBookEntry []) tickSize =
-    for i in orderBook.Length-1 .. -1 .. orderBookUpdate.PriceLevelIndex  do
-        orderBook.[i] <- orderBook.[i - 1]
-
-    orderBook.[orderBookUpdate.PriceLevelIndex - 1] <- OrderBookEntry.Update ((decimal)orderBookUpdate.Price * tickSize) orderBookUpdate.Quantity
-
-// O(n)
-// to improve look at 
-// https://hamberg.no/erlend/posts/2012-08-29-purely-functional-random-access-list.html
-// https://gist.github.com/kos59125/3721051
-let insertIntoOrderBook2 orderBookUpdate (orderBook:OrderBookEntry []) tickSize =
-    orderBook 
-    |> Array.rev
-    |> Array.mapi (
-        fun idx elem -> 
-            match idx with
-            | idx when idx < (orderBook.Length - orderBookUpdate.PriceLevelIndex) -> orderBook.[idx + 1]
-            | _ -> elem )
-    |> Array.rev
-    |> updateOrderBook2 orderBookUpdate tickSize
-
-// O(n)
-let deleteFromOrderBook orderBookUpdate (orderBook:OrderBookEntry []) =
-    orderBook |> Array.iteri (fun idx elem -> 
-                                                if(idx < orderBook.Length - 1) then 
-                                                    if(idx >= (orderBookUpdate.PriceLevelIndex - 1)) then 
-                                                        orderBook.[idx] <- orderBook.[idx + 1]
-                                                else
-                                                    orderBook.[idx] <- OrderBookEntry.Default)
-
-// O(n)
-let deleteFromOrderBook2 orderBookUpdate (orderBook:OrderBookEntry []) =
-
-    orderBook 
-    |> Array.mapi (
-        fun idx elem -> 
-                        match idx with
-                        | idx when idx = (orderBook.Length - 1) -> OrderBookEntry.Default
-                        | idx when idx >= (orderBookUpdate.PriceLevelIndex - 1) -> orderBook.[idx + 1]
-                        | _ -> elem )
+open QuanTemplateTest.OrderBook
 
 [<EntryPoint>]
 let main argv = 
-    
-    let fileName = argv.[0]
-    let tickSize = Convert.ToDecimal(argv.[1])
-    let bookDepth = Convert.ToInt32(argv.[2])
+    try
+        let fileName = argv.[0]
+        let tickSize = Convert.ToDecimal(argv.[1])
+        let bookDepth = Convert.ToInt32(argv.[2])
 
-    let bidOrderBook = Array.create bookDepth OrderBookEntry.Default
-    let askOrderBook = Array.create bookDepth OrderBookEntry.Default
+        let orderBookChanges = IO.File.ReadLines(fileName)
 
-    let orderBookChanges = IO.File.ReadLines(fileName)
-
-   
-
-    let updateOrderBookFromOrderBookUpdate orderBookUpdate =
-        match orderBookUpdate.Side with
-        | Bid ->
-            match orderBookUpdate.Instruction with
-            | Update -> updateOrderBook orderBookUpdate bidOrderBook tickSize
-            | Delete -> deleteFromOrderBook orderBookUpdate bidOrderBook 
-            | New -> insertIntoOrderBook orderBookUpdate bidOrderBook tickSize
-        | Ask ->
-            match orderBookUpdate.Instruction with
-            | Update -> updateOrderBook orderBookUpdate askOrderBook tickSize
-            | Delete -> deleteFromOrderBook orderBookUpdate askOrderBook
-            | New -> insertIntoOrderBook orderBookUpdate askOrderBook tickSize
-
-    orderBookChanges |> Seq.iter (OrderBookUpdate.FromOrderBookUpdateFileLine >> updateOrderBookFromOrderBookUpdate)
-
-    Array.iter2 (fun x y -> printfn "%.1f,%d,%.1f,%d" x.Price x.Quantity y.Price y.Quantity) bidOrderBook askOrderBook
+        let orderBook = new ArrayBasedOrderBook(tickSize, bookDepth)
+        orderBook.ProcessChanges (orderBookChanges)
+        orderBook.Print ()
+    with
+        | :? System.IO.FileNotFoundException as fnfex -> printfn "Filename \"%s\" was not found." argv.[0]
+        | :? System.Exception as ex -> printfn "%s" ex.Message
 
     0 // return an integer exit code
